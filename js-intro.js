6771329b32774a634082683ed0f051916378a502
Ubuntu <ubuntu@ip-172-31-2-143.us-east-2.compute.internal>
2019-06-07 22:40:51 +0000

    assessment answers

--- js-intro.js
+++ js-intro.js
@@ -6,17 +6,28 @@ var mantra = "Be the dev";
 
 // 1. Write the code that returns 'B' from mantra.
 
-// 2. Write the code that determines if there is a 'x' in mantra.
+console.log(mantra[0])
+
+// OR
+
+findLetter = (words) => words.charAt(0)
+
+// 2. Write the code that determines if there is an 'x' in mantra.
+
+console.log(mantra.includes("x"))
 
 // 3. Write the code that determines if there is a 'v' in mantra.
 
+console.log(mantra.includes("v"))
+
 // Stretch: Write the code that returns the position of 'v' in mantra.
 
+console.log(mantra.indexOf("v"))
 
 // Consider the following statement:
 
-var codingIsEasy
-var imBored
+var codingIsEasy = true
+var imBored = true
 
 if (codingIsEasy && imBored) {
     console.log("I need more!")
@@ -26,10 +37,12 @@ if (codingIsEasy && imBored) {
 
 // 4. Update the codingIsEasy and imBored variables above the statement to the values required to log "I need more!"
 
+//done
+
 // Consider the following statement:
 
-var imLost
-var imFrustrated
+var imLost = false
+var imFrustrated = false
 
 if (imLost || imFrustrated) {
     console.log('Break time!')
@@ -39,7 +52,14 @@ if (imLost || imFrustrated) {
 
 // 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
 
+//done
+
 // 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
+
+var mantra = "Be the dev"
+for(let i=0; i<mantra.length; i++){
+    console.log(mantra[i])
+}
 
 // Stretch: Using a while loop.
 
@@ -47,9 +67,26 @@ if (imLost || imFrustrated) {
 
 // Consider the following variable:
 
-var message = "thisisateststring"
+var testString = "thisisateststring"
+var newTestString = 12345
 
 // 7. Write a function that takes a string like message as an argument and returns the string without vowels.
+
+
+const noVowels = (words) =>{
+    if(words != "string"){
+        return "Please only use words"
+    } else {
+            var newString = ""
+            for(let i=0; i<words.length; i++){
+            if(words[i] !="a" && words[i] !="e" && words[i] !="i" && words[i] !="o" && words[i] !="u"){
+            newString += words[i]
+            }
+    }
+    return newString
+    }
+}
+console.log(noVowels(newTestString))
 
 // Stretch: Update your function to throw an exception if the argument is not a string.
 
@@ -60,3 +97,14 @@ var message = "thisisateststring"
 var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
 
 // 8. Write a function that takes an array like toonimals and returns an array with only the toons that are cats.
+
+findCats = (arr) => {
+    var onlyCats = []
+    for(i=0;i<arr.length;i++){
+        if(arr[i].animal === "cat"){
+            onlyCats.push(arr[i])
+        }
+    }
+    return onlyCats
+}
+console.log(findCats(toonimals))
